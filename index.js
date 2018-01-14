e5905674fbcaccb758649828f7ccd9109430d3af
Andrew McIntosh <andrewdavidmcintosh@gmail.com>
2018-01-14 18:31:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 var katzDeli = [];
 
 takeANumber(katzDeli, "Ada");
@@ -9,6 +10,20 @@ takeANumber(katzDeli, "Matz");
 currentLine(katzDeli); 
 nowServing(katzDeli);
 currentLine(katzDeli);
+=======
+<<<<<<< HEAD
+
+function takeANumber(katzDeliLine) {
+   var katzDeli = [];
+=======
+function takeANumber(katzDeliLine, katzDeli) {
+ 
+>>>>>>> 89bbcd2e6886214654cf61c7c8efdd26a225ac3b
+  var nextGuest = ['Ada', 'Grace', 'Kent'];
+  for (var i = 1; i < katzDeli.length; i++);
+  katzDeli.push(nextGuest[i]);
+  return ('Welcome, ' + nextGuest[i] + '. You are number ' + i + ' in line.');
+>>>>>>> 85ba8c8bed10ae36239476876e3b2e6e8d015339
 
 function currentLine(katzDeliLine){
   if (katzDeliLine.length === 0) {
